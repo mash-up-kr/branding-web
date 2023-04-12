7d65f974ca732c3a7956ddc3f110e044e44bc615
HaJunRyu <hjr4557@naver.com>
2023-04-13 01:44:25 +0900

    Env: eslint, prettier 초기 설정을 한다
    기존 mashup-recruit repo에서 사용하던것을 이관했다

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,6 +1,27 @@
 module.exports = {
-  extends: ['next', 'turbo', 'prettier'],
+  extends: ['airbnb', 'airbnb/hooks', 'next', 'turbo', 'prettier'],
+  plugins: ['prettier'],
   rules: {
+    'prettier/prettier': 'error',
     '@next/next/no-html-link-for-pages': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/jsx-props-no-spreading': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/no-array-index-key': 'off',
+    'react/require-default-props': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-cycle': 'off',
+    'no-throw-literal': 'off',
+    'no-nested-ternary': 'off',
   },
 };
