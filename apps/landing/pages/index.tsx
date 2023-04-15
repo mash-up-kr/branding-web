97e87bedc7308f6d7d25c6662ba4bea6a41314b6
HaJunRyu <hjr4557@naver.com>
2023-04-15 15:42:35 +0900

    Feat: Landing app을 추가한다

---
+++ apps/landing/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'ui';
+
+const Home = () => (
+  <div>
+    <h1>Landing</h1>
+    <Button />
+  </div>
+);
+
+export default Home;
--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,5 +1,1 @@
-import * as React from "react";
-
-export const Button = () => {
-  return <button>Boop</button>;
-};
+export const Button = () => <button>Boop</button>;
