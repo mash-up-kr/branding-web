8d2d38b133e21ce1f924be580fd8083b9abacd5a
HaJunRyu <hjr4557@naver.com>
2023-04-16 19:02:44 +0900

    Env: landing app에 emotion 의존성을 추가한다

--- apps/landing/next.config.js
+++ apps/landing/next.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ['ui'],
+  compiler: {
+    emotion: {
+      autoLabel: 'dev-only',
+      labelFormat: '[local]',
+    },
+  },
 };
