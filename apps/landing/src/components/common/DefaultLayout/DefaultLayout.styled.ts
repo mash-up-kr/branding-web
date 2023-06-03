83ff3dd01e5ec04074c4984c11e1b3812f93ce21
HaJunRyu <hjr4557@naver.com>
2023-06-03 14:58:57 +0900

    Feat: DefaultLayout의 뷰포트별 padding을 정의한다

--- apps/landing/src/components/common/DefaultLayout/DefaultLayout.styled.ts
+++ apps/landing/src/components/common/DefaultLayout/DefaultLayout.styled.ts
@@ -1,7 +1,26 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 export const Layout = styled.div`
-  width: 144rem;
-  margin: 0 auto;
-  padding: 0 8rem;
+  ${({ theme }) => css`
+    width: 144rem;
+    margin: 0 auto;
+    padding: 0 8rem;
+
+    @media (max-width: ${theme.breakPoint.media.notebook}) {
+      width: 100%;
+    }
+
+    @media (max-width: ${theme.breakPoint.media.tabletL}) {
+      padding: 0 6rem;
+    }
+
+    @media (max-width: ${theme.breakPoint.media.tabletS}) {
+      padding: 0 3rem;
+    }
+
+    @media (max-width: ${theme.breakPoint.media.mobile}) {
+      padding: 0 2rem;
+    }
+  `}
 `;
