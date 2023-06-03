053085e2c04e4dcaf0b4cc1a52c50309ce859446
HaJunRyu <hjr4557@naver.com>
2023-06-03 11:10:16 +0900

    Feat: index 페이지에 main 레벨의 컴포넌트를 렌더링한다

---
+++ apps/landing/src/components/common/Main/Main.component.tsx
@@ -0,0 +1,3 @@
+const Main = () => <main>{}</main>;
+
+export default Main;
--- apps/landing/src/components/common/index.ts
+++ apps/landing/src/components/common/index.ts
@@ -2,3 +2,4 @@ export { default as DefaultLayout } from './DefaultLayout/DefaultLayout.componen
 export { default as Heading } from './Heading/Heading.component';
 export { default as Navigation } from './Navigation/Navigation.component';
 export { default as Header } from './Header/Header.component';
+export { default as Main } from './Main/Main.component';
