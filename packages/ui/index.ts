8b42db5f53d5c64f2e798e352596aba8ecb2751b
HaJunRyu <hjr4557@naver.com>
2023-07-01 16:52:19 +0900

    Feat: Lottie ui component를 정의한다

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,2 +1,3 @@
 export { default as MashUpLogo } from './MashUpLogo/MashUpLogo.component';
 export { default as LinearGradientSphere } from './LinearGradientSphere/LinearGradientSphere.component';
+export { default as Lottie } from './Lottie/Lottie';
