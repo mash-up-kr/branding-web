1b581de5737873534b52f284cac60c6d83f55111
sanoopark <sanoopark@gmail.com>
2024-06-08 22:07:34 +0900

    Refactor: alt optional type을 추가한다

--- apps/webview/ui/svg-image.tsx
+++ apps/webview/ui/svg-image.tsx
@@ -5,11 +5,12 @@ import { forwardRef, type ComponentPropsWithoutRef } from 'react';
 type SvgImageProps = Omit<ComponentPropsWithoutRef<typeof Image>, 'src' | 'alt'> & {
   basePath?: string;
   path: string;
+  alt?: string;
 };
 
 const SvgImage = forwardRef<HTMLImageElement, SvgImageProps>(
-  ({ basePath = 'mashong', path, ...restProps }, ref) => (
-    <Image ref={ref} src={createSvgUrl(basePath)(path)} alt="" unoptimized {...restProps} />
+  ({ basePath = 'mashong', path, alt = '', ...restProps }, ref) => (
+    <Image ref={ref} src={createSvgUrl(basePath)(path)} alt={alt} unoptimized {...restProps} />
   ),
 );
 
