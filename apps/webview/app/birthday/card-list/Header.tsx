8c25988add981680c3de1ff0608a2b7954ddb6e5
Baek2back <dumbuk12@naver.com>
2024-08-05 14:47:59 +0900

    Feat: 정의한 웹뷰 핸들러를 적용한다

--- apps/webview/app/birthday/card-list/Header.tsx
+++ apps/webview/app/birthday/card-list/Header.tsx
@@ -1,43 +1,44 @@
 'use client';
 
+import { useWebviewHandler } from '@/hooks/useWebviewHandler';
 import { styled } from '@/styled-system/jsx';
 import SvgImage from '@/ui/svg-image';
 
-const Header = () => (
-  <styled.header
-    height="56px"
-    position="fixed"
-    maxW="[768px]"
-    w="100%"
-    left="[50%]"
-    translate="auto"
-    translateX="-1/2"
-    zIndex="1"
-  >
-    <styled.div
-      display="flex"
-      flexDirection="row"
-      justifyContent="end"
-      alignItems="center"
-      px="20px"
-      py="16px"
+const Header = () => {
+  const webviewHandler = useWebviewHandler();
+
+  return (
+    <styled.header
+      height="56px"
+      position="fixed"
+      maxW="[768px]"
+      w="100%"
+      left="[50%]"
+      translate="auto"
+      translateX="-1/2"
+      zIndex="1"
     >
-      <SvgImage
-        basePath="birthday"
-        path="common/close"
-        width={24}
-        height={24}
-        fill={false}
-        onClick={() => {
-          // TODO: iOS, AOS 공통 브릿지 정의 예정
-          // @ts-ignore
-          window.webkit.messageHandlers.mashupBridge.postMessage({
-            step: 'back',
-          });
-        }}
-      />
-    </styled.div>
-  </styled.header>
-);
+      <styled.div
+        display="flex"
+        flexDirection="row"
+        justifyContent="end"
+        alignItems="center"
+        px="20px"
+        py="16px"
+      >
+        <SvgImage
+          basePath="birthday"
+          path="common/close"
+          width={24}
+          height={24}
+          fill={false}
+          onClick={() => {
+            webviewHandler.step('back');
+          }}
+        />
+      </styled.div>
+    </styled.header>
+  );
+};
 
 export default Header;
