65ca07ceebb2d9f381d1afa3d46697725ae63507
kikiyeom <hsyeom9292@gmail.com>
2024-08-07 13:10:39 +0900

    Feat: 생일 축하 기능을 구현하다

---
+++ apps/webview/app/_actions/createCard.ts
@@ -0,0 +1,42 @@
+'use server';
+
+import { cookies } from 'next/headers';
+
+export const createCard = async ({
+  id,
+  message,
+  imageUrl,
+}: {
+  id: string;
+  message: string;
+  imageUrl: string;
+}) => {
+  try {
+    const authToken = cookies().get('token');
+
+    if (!authToken) {
+      throw new Error(`유효한 인증 토큰이 필요합니다.`);
+    }
+
+    const res = await fetch(`https://api.dev-member.mash-up.kr/api/v1/birthday-cards`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${authToken.value}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        recipientMemberId: Number(id),
+        imageUrl,
+        message,
+      }),
+    });
+
+    const { data } = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+    };
+  }
+};
