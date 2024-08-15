d9b1e3ab986b77a190aad0c8d06099f0fe95a1b0
sanoopark <sanoopark@gmail.com>
2024-08-15 20:01:57 +0900

    Fix: 서버 컴포넌트에서 searchParams를 제거한다
    - Layout을 사용하지 못하는 문제

---
+++ apps/webview/app/mashong/growth-diary/_components/InfoBadge.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { PLATFORM_NAME_MAP } from '@/../../packages/constant';
+import { useSearchParams } from 'next/navigation';
+import { PropsWithChildren } from 'react';
+
+import { styled } from '@/styled-system/jsx';
+
+export const InfoBadges = () => {
+  const searchParams = useSearchParams();
+  const currentLevel = searchParams.get('level');
+  const currentPlatform = searchParams
+    .get('platform')
+    ?.toUpperCase() as keyof typeof PLATFORM_NAME_MAP;
+
+  return (
+    <styled.div display="flex" gap={8}>
+      <InfoBadge>Lv.{currentLevel}</InfoBadge>
+      <InfoBadge>{PLATFORM_NAME_MAP[currentPlatform]}</InfoBadge>
+    </styled.div>
+  );
+};
+
+const InfoBadge = ({ children }: PropsWithChildren) => (
+  <styled.span
+    borderRadius={30}
+    padding="3.5px 12px"
+    bg="gray.100"
+    fontWeight={500}
+    fontSize={14}
+    letterSpacing="-1%"
+    lineHeight="16.7px"
+    color="gray.600"
+  >
+    {children}
+  </styled.span>
+);
--- apps/webview/app/mashong/growth-diary/page.tsx
+++ apps/webview/app/mashong/growth-diary/page.tsx
@@ -1,84 +1,55 @@
-import { PLATFORM_NAME_MAP } from 'constant';
-import { PropsWithChildren } from 'react';
-
 import { styled } from '@/styled-system/jsx';
 
+import { InfoBadges } from './_components/InfoBadge';
 import { LevelCarousel } from './_components/LevelCarousel';
 import { TopNavigationButton } from '../[team]/_components/TopNavigationButton';
 
-interface SearchParams {
-  level: number;
-  platform: keyof typeof PLATFORM_NAME_MAP;
-}
-
-const Page = async ({ searchParams }: { searchParams: SearchParams }) => {
-  const currentLevel = searchParams.level;
-  const currentPlatform = searchParams.platform.toUpperCase() as keyof typeof PLATFORM_NAME_MAP;
-
-  return (
-    <styled.div display="flex" flexDirection="column" justifyContent="space-around" h="80dvh">
-      <styled.div display="flex" justifyContent="space-between" alignItems="center">
-        <TopNavigationButton />
-        <styled.span
-          fontWeight={600}
-          fontSize={16}
-          letterSpacing="-1%"
-          color="gray.900"
-          userSelect="none"
-        >
-          성장 일기
-        </styled.span>
-      </styled.div>
-
-      <styled.div
-        w="100%"
-        minH={450}
-        bg="white"
-        padding={20}
-        borderRadius={20}
-        m="20px auto 29px auto"
+const Page = async () => (
+  <styled.div display="flex" flexDirection="column" justifyContent="space-around" h="80dvh">
+    <styled.div display="flex" justifyContent="space-between" alignItems="center">
+      <TopNavigationButton />
+      <styled.span
+        fontWeight={600}
+        fontSize={16}
+        letterSpacing="-1%"
+        color="gray.900"
+        userSelect="none"
       >
-        <styled.div display="flex" gap={8}>
-          <InfoLabel>Lv.{currentLevel}</InfoLabel>
-          <InfoLabel>{PLATFORM_NAME_MAP[currentPlatform]}</InfoLabel>
-        </styled.div>
-        <styled.strong
-          fontWeight={700}
-          fontSize={30}
-          lineHeight="35.8px"
-          letterSpacing="-1%"
-          padding="20px 0"
-          display="inline-block"
-          color="gray.950"
-        >
-          나는 매쉬업에 <br />
-          태어나버렸다.
-        </styled.strong>
-      </styled.div>
-
-      <div>
-        <styled.div color="gray.700" fontWeight={700} fontSize={16} letterSpacing="-1%" mb={10}>
-          내 일기
-        </styled.div>
-        <LevelCarousel />
-      </div>
+        성장 일기
+      </styled.span>
     </styled.div>
-  );
-};
+
+    <styled.div
+      w="100%"
+      minH={450}
+      bg="white"
+      padding={20}
+      borderRadius={20}
+      m="20px auto 29px auto"
+    >
+      <InfoBadges />
+
+      <styled.strong
+        fontWeight={700}
+        fontSize={30}
+        lineHeight="35.8px"
+        letterSpacing="-1%"
+        padding="20px 0"
+        display="inline-block"
+        color="gray.950"
+      >
+        나는 매쉬업에 <br />
+        태어나버렸다.
+      </styled.strong>
+    </styled.div>
+
+    <div>
+      <styled.div color="gray.700" fontWeight={700} fontSize={16} letterSpacing="-1%" mb={10}>
+        내 일기
+      </styled.div>
+      <LevelCarousel />
+    </div>
+  </styled.div>
+);
 
 export default Page;
-
-const InfoLabel = ({ children }: PropsWithChildren) => (
-  <styled.span
-    borderRadius={30}
-    padding="3.5px 12px"
-    bg="gray.100"
-    fontWeight={500}
-    fontSize={14}
-    letterSpacing="-1%"
-    lineHeight="16.7px"
-    color="gray.600"
-  >
-    {children}
-  </styled.span>
-);
