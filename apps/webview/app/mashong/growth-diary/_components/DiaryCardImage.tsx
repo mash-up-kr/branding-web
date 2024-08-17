a2e8ebd3293efa258ff0398252edf7b93e2c8ebb
sanoopark <sanoopark@gmail.com>
2024-08-17 19:00:30 +0900

    Feat: 이미지 옵션을 추가한다

--- apps/webview/app/mashong/growth-diary/_components/DiaryCardImage.tsx
+++ apps/webview/app/mashong/growth-diary/_components/DiaryCardImage.tsx
@@ -7,5 +7,5 @@ import SvgImage from '@/ui/svg-image';
 export const DiaryCardImage = () => {
   const searchParams = useSearchParams();
   const currentLevel = searchParams.get('level');
-  return <SvgImage path={`growth-diary/card/lv${currentLevel}`} fill />;
+  return <SvgImage path={`growth-diary/card/lv${currentLevel}`} fill priority quality={100} />;
 };
