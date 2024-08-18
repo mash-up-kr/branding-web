6c23e907c3330785a61bef8572ea4992c0a4e0f8
sanoopark <sanoopark@gmail.com>
2024-08-18 10:09:55 +0900

    Fix: 토스트 스타일을 수정한다

--- apps/webview/app/_components/ErrorToast.tsx
+++ apps/webview/app/_components/ErrorToast.tsx
@@ -19,5 +19,5 @@ export const ErrorToast = ({ message }: { message: ReactNode }) => (
 );
 
 export const showErrorToast = (message: ReactNode) => {
-  toast.custom(<ErrorToast message={message} />, { duration: 3000 });
+  toast.custom(<ErrorToast message={message} />, { duration: 300000000 });
 };
--- apps/webview/app/mashong/[team]/_components/PopcornToast.tsx
+++ apps/webview/app/mashong/[team]/_components/PopcornToast.tsx
@@ -5,7 +5,15 @@ import { toast } from 'react-hot-toast/headless';
 import { styled } from '@/styled-system/jsx';
 
 const PopcornToast = ({ value }: { value: number }) => (
-  <styled.span color="white" textAlign="center">
+  <styled.span
+    color="white"
+    textAlign="center"
+    fontWeight={500}
+    fontSize={14}
+    lineHeight="16.7px"
+    letterSpacing="-1%"
+    whiteSpace="nowrap"
+  >
     <span>매숑이가 </span>
     <styled.span fontWeight={700} color="brand.300">
       팝콘 {value}개
