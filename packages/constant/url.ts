7f276c11ed502b26972caa0bf424d9f473103700
sanoopark <sanoopark@gmail.com>
2024-08-18 10:42:17 +0900

    Fix: 레벨업 로띠 URL을 수정한다

--- packages/constant/url.ts
+++ packages/constant/url.ts
@@ -1,3 +1,3 @@
-export const STATIC_ORIGIN = 'static.mash-up.kr';
+export const STATIC_ORIGIN = 'https://static.mash-up.kr';
 export const createSvgUrl = (basePath: string) => (path: string) =>
-  `//${STATIC_ORIGIN}/images/svg/${basePath}/${path}.svg`;
+  `${STATIC_ORIGIN}/images/svg/${basePath}/${path}.svg`;
