8ba9ad6fd5acb53512e7f64d216da7953a890dbc
HajunRyu(류하준) <71176945+HaJunRyu@users.noreply.github.com>
2024-08-18 22:57:10 +0900

    랜딩 페이지 Activity Section 추가 (#93)
    * Feat: scroll-snap을 on/off 할 수 있도록 한다
    
    * Feat: ActivitySection의 기본 UI를 구성한다
    
    * Feat: ActivityItem의 문구 및 사진을 실제 데이터로 교체한다
    
    * Feat: Activity Section title의 position을 변경한다
    
    * Feat: ActivitySection에 각 뷰포트별 반응형 스타일을 적용한다

--- apps/landing/src/components/common/SnapParent/SnapParent.component.tsx
+++ apps/landing/src/components/common/SnapParent/SnapParent.component.tsx
@@ -6,8 +6,8 @@ interface SnapParentProps extends Styled.SnapParentProps {
   children: ReactNode;
 }
 
-const SnapParent = ({ axis, strictness, children }: SnapParentProps) => (
-  <Styled.SnapParent axis={axis} strictness={strictness}>
+const SnapParent = ({ axis, strictness, disabled, children }: SnapParentProps) => (
+  <Styled.SnapParent axis={axis} strictness={strictness} disabled={disabled}>
     {children}
   </Styled.SnapParent>
 );
--- apps/landing/src/components/main/index.ts
+++ apps/landing/src/components/main/index.ts
@@ -4,3 +4,5 @@ export { default as IntroTyping } from './IntroTyping/IntroTyping.component';
 export { default as IntroTypingMobile } from './IntroTypingMobile/IntroTypingMobile.component';
 export { default as PlatformIntroduceSection } from './PlatformIntroduceSection/PlatformIntroduceSection.component';
 export { default as JoinRecruitCard } from './JoinRecruitCard/JoinRecruitCard.component';
+export { default as ActivitySection } from './ActivitySection/ActivitySection.component';
+export { default as ActivityItem } from './ActivityItem/ActivityItem.component';
