73b805f112204e52570479a44cf8dc4faa82a006
sanoopark <sanoopark@gmail.com>
2024-08-21 01:54:57 +0900

    Feat: 잠긴 버튼 클릭할 때 Shake 애니메이션을 추가한다

--- apps/webview/app/mashong/growth-diary/_components/LevelCarousel.tsx
+++ apps/webview/app/mashong/growth-diary/_components/LevelCarousel.tsx
@@ -1,5 +1,6 @@
 import { EmblaViewportRefType } from 'embla-carousel-react';
-import React from 'react';
+import React, { useState } from 'react';
+import { useDebounceCallback } from 'usehooks-ts';
 
 import { styled } from '@/styled-system/jsx';
 import SvgImage from '@/ui/svg-image';
@@ -27,7 +28,10 @@ export const LevelCarousel = ({
           isActive={selectedIndex + 1 === level}
           isLocked={currentLevel < level}
           onClick={() => {
-            if (level > currentLevel) return;
+            if (currentLevel < level) {
+              // shake 애니메이션 필요
+              return;
+            }
             onClick(level - 1);
           }}
         />
@@ -43,28 +47,50 @@ interface LevelButtonProps {
   onClick: () => void;
 }
 
-const LevelButton = ({ level, isActive = false, isLocked = false, onClick }: LevelButtonProps) => (
-  <styled.button type="button" onClick={onClick} cursor="pointer">
-    <styled.div
-      width={60}
-      height={60}
-      backgroundColor={isLocked ? 'gray.100' : isActive ? 'brand.100' : 'white'}
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      borderRadius={10}
-      borderWidth="1.5px"
-      borderColor={isActive ? 'brand.500' : 'transparent'}
-      userSelect="none"
-      transition="all ease 400ms"
+const LevelButton = ({ level, isActive = false, isLocked = false, onClick }: LevelButtonProps) => {
+  const [shake, setShake] = useState(false);
+
+  const debouncedShake = useDebounceCallback(() => {
+    setShake(true);
+    setTimeout(() => setShake(false), 700);
+  }, 200);
+
+  const handleClick = () => {
+    if (isLocked) {
+      debouncedShake();
+    } else {
+      onClick();
+    }
+  };
+
+  return (
+    <styled.button
+      type="button"
+      onClick={handleClick}
+      cursor="pointer"
+      animation={shake ? 'shake 0.7s ease' : ''}
     >
-      {isLocked ? (
-        <SvgImage path="growth-diary/level-lock" width={32} height={32} />
-      ) : (
-        <styled.span fontWeight={700} fontSize={16} color={isActive ? 'brand.500' : 'gray.400'}>
-          Lv.{level}
-        </styled.span>
-      )}
-    </styled.div>
-  </styled.button>
-);
+      <styled.div
+        width={60}
+        height={60}
+        backgroundColor={isLocked ? 'gray.100' : isActive ? 'brand.100' : 'white'}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        borderRadius={10}
+        borderWidth="1.5px"
+        borderColor={isActive ? 'brand.500' : 'transparent'}
+        userSelect="none"
+        transition="all ease 400ms"
+      >
+        {isLocked ? (
+          <SvgImage path="growth-diary/level-lock" width={32} height={32} />
+        ) : (
+          <styled.span fontWeight={700} fontSize={16} color={isActive ? 'brand.500' : 'gray.400'}>
+            Lv.{level}
+          </styled.span>
+        )}
+      </styled.div>
+    </styled.button>
+  );
+};
--- apps/webview/panda.config.ts
+++ apps/webview/panda.config.ts
@@ -71,6 +71,20 @@ export default defineConfig({
           transform: 'scale(1.03)',
         },
       },
+      shake: {
+        '0%, 100%': {
+          transform: 'translateX(0)',
+        },
+        '25%': {
+          transform: 'translateX(-5px)',
+        },
+        '50%': {
+          transform: 'translateX(5px)',
+        },
+        '75%': {
+          transform: 'translateX(-5px)',
+        },
+      },
     },
   },
 
