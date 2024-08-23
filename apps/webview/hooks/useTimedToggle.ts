9287d7fe8f18cce298b7f0766c8f5b6ea308d4fe
sanoopark <sanoopark@gmail.com>
2024-08-23 21:24:04 +0900

    Fix: 말풍선 오동작을 수정한다
    - 이전 타이머 제거하는 로직 추가

--- apps/webview/hooks/useTimedToggle.ts
+++ apps/webview/hooks/useTimedToggle.ts
@@ -1,21 +1,25 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useDebounceCallback, useTimeout } from 'usehooks-ts';
 
 export const useTimedToggle = ({ debounceDelay = 200, activeDuration = 2000 }) => {
   const [isToggled, setIsToggled] = useState(false);
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
 
-  const debouncedSetIsToggled = useDebounceCallback((_isToggled) => {
-    setIsToggled(_isToggled);
+  const debouncedTriggerToggle = useDebounceCallback(() => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+    triggerToggle();
   }, debounceDelay);
 
   const triggerToggle = () => {
-    debouncedSetIsToggled(true);
-    setTimeout(() => setIsToggled(false), activeDuration);
+    setIsToggled(true);
+    timeoutRef.current = setTimeout(() => setIsToggled(false), activeDuration);
   };
 
   useTimeout(() => {
     triggerToggle();
-  }, debounceDelay);
+  }, 200);
 
-  return { isToggled, triggerToggle };
+  return { isToggled, triggerToggle: debouncedTriggerToggle };
 };
