199bf7ed39314c09bf757ed7dbc0034bd516b1ba
HajunRyu(류하준) <71176945+HaJunRyu@users.noreply.github.com>
2024-08-25 23:23:55 +0900

    랜딩페이지 mobile 뷰포트 interaction을 구현한다 (#118)
    * Feat: IntroSection의 mobile interaction을 구현한다
    
    * Feat: IntroTypingSection의 mobile interaction을 구현한다
    
    * Style: MashongSection 설명 UI의 width를 뷰포트별로 조정한다

--- apps/landing/src/components/main/IntroTypingMobile/IntroTypingMobile.styled.ts
+++ apps/landing/src/components/main/IntroTypingMobile/IntroTypingMobile.styled.ts
@@ -1,4 +1,4 @@
-import { css } from '@emotion/react';
+import { css, keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
 
 export const TypingContainer = styled.div`
@@ -17,23 +17,85 @@ export const TypingContainer = styled.div`
   `}
 `;
 
-export const TextRelative = styled.div`
-  position: relative;
-`;
+export const TextWrapper = styled.div``;
 
-interface MaskLineProps {
-  isMaskLineIntersect: boolean;
+interface fillWidthProps {
+  duration: number;
+  delay?: number;
+  timingFunction?: string;
+  isInView?: boolean;
 }
 
-export const MaskLine = styled.div<MaskLineProps>`
-  ${({ isMaskLineIntersect }) => css`
-    position: absolute;
-    top: 0;
-    right: 0;
-    width: ${isMaskLineIntersect ? '0%' : '100%'};
-    height: 100%;
-    background: #000000;
-    opacity: 0.9;
-    transition: ${isMaskLineIntersect ? '2s linear' : 'none'};
+const fillWidth = ({
+  duration,
+  delay = 0,
+  timingFunction = 'linear',
+  isInView = true,
+}: fillWidthProps) => {
+  const fillWidthAnimation = keyframes`
+  from {
+    width: 100%;
+  }
+  to {
+    width: 0%;
+  }
+`;
+
+  return css`
+    width: 100%;
+    animation: ${isInView ? fillWidthAnimation : null} ${duration}s ${delay}s ${timingFunction}
+      forwards;
+  `;
+};
+
+export const TextRelative = styled.div<{ isInView: boolean }>`
+  ${({ theme, isInView }) => css`
+    position: relative;
+
+    &:first-of-type {
+      ${theme.animation.fadeUp({ duration: 0.8, delay: 0.01, move: '3rem', isInView })};
+
+      & > div {
+        ${fillWidth({ duration: 2, delay: 0.01, isInView })};
+      }
+    }
+    &:nth-of-type(2) {
+      ${theme.animation.fadeUp({ duration: 0.8, delay: 0.4, move: '3rem', isInView })};
+
+      & > div {
+        ${fillWidth({ duration: 2, delay: 0.4, isInView })};
+      }
+    }
+    &:nth-of-type(3) {
+      ${theme.animation.fadeUp({ duration: 0.8, delay: 0.8, move: '3rem', isInView })};
+
+      & > div {
+        ${fillWidth({ duration: 2, delay: 0.8, isInView })};
+      }
+    }
+    &:nth-of-type(4) {
+      ${theme.animation.fadeUp({ duration: 0.8, delay: 1.2, move: '3rem', isInView })};
+
+      & > div {
+        ${fillWidth({ duration: 2, delay: 1.2, isInView })};
+      }
+    }
+    &:nth-of-type(5) {
+      ${theme.animation.fadeUp({ duration: 0.8, delay: 1.6, move: '3rem', isInView })};
+
+      & > div {
+        ${fillWidth({ duration: 2, delay: 1.6, isInView })};
+      }
+    }
   `}
+`;
+
+export const MaskLine = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  background: #000000;
+  opacity: 0.9;
 `;
