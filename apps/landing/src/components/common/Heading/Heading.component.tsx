e6efe826714594061bbb9a45432b42ea52997fbf
HaJunRyu <hjr4557@naver.com>
2024-08-25 23:38:24 +0900

    Fix: logo 크기가 깜빡이는 현상을 media query를 사용하여 해결한다

--- apps/landing/src/components/common/Heading/Heading.component.tsx
+++ apps/landing/src/components/common/Heading/Heading.component.tsx
@@ -1,29 +1,14 @@
-import { MashUpLogo } from 'ui';
-
-import { ROUTES, TViewPortSize } from '@/constants';
-import { useDetectViewport } from '@/hooks';
+import { ROUTES } from '@/constants';
 
 import * as Styled from './Heading.styled';
 
-const MASH_UP_LOGO_SIZE: Readonly<Record<TViewPortSize, number>> = {
-  DESKTOP: 44,
-  NOTEBOOK: 44,
-  TABLET_L: 36,
-  TABLET_S: 24,
-  MOBILE: 24,
-};
-
-const Heading = () => {
-  const { viewportSize } = useDetectViewport();
-
-  return (
-    <h1>
-      <Styled.LinkToHome href={ROUTES.HOME}>
-        <MashUpLogo size={MASH_UP_LOGO_SIZE[viewportSize]} />
-        <Styled.MashUp>Mash-Up</Styled.MashUp>
-      </Styled.LinkToHome>
-    </h1>
-  );
-};
+const Heading = () => (
+  <h1>
+    <Styled.LinkToHome href={ROUTES.HOME}>
+      <Styled.MashUpLogo />
+      <Styled.MashUp>Mash-Up</Styled.MashUp>
+    </Styled.LinkToHome>
+  </h1>
+);
 
 export default Heading;
--- apps/landing/src/components/common/Heading/Heading.styled.ts
+++ apps/landing/src/components/common/Heading/Heading.styled.ts
@@ -1,6 +1,7 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import Link from 'next/link';
+import { MashUpLogo as MashUpLogoComponent } from 'ui';
 
 export const LinkToHome = styled(Link)`
   display: flex;
@@ -19,6 +20,22 @@ export const MashUp = styled.span`
 
     @media (max-width: ${theme.breakPoint.media.tabletS}) {
       display: none;
+    }
+  `}
+`;
+
+export const MashUpLogo = styled(MashUpLogoComponent)`
+  ${({ theme }) => css`
+    width: 4.4rem;
+    height: 4.4rem;
+
+    @media (max-width: ${theme.breakPoint.media.tabletL}) {
+      width: 3.6rem;
+      height: 3.6rem;
+    }
+    @media (max-width: ${theme.breakPoint.media.tabletS}) {
+      width: 2.4rem;
+      height: 2.4rem;
     }
   `}
 `;
